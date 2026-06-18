d8a32dccf40b714c9e393c75cded1470ce96cf44
rajacho29-ctrl <rajacho29@gmail.com>
2026-06-18 22:57:38 +0000

    modifique test.py

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 
 import requests
+import pandas as pd
 url = "https://books.toscrape.com/"
 headers = {"User-Agent": "Mozilla/5.0"}
 r = requests.get(url, headers=headers, timeout=10)
 print(r.status_code) # 200 -> todo bien
 print(len(r.text), "caracteres de HTML")
+
+
